e7c4caf8e90d2d9b5a703dcca126044c1f2c2d24
= <=>
2025-07-14 11:53:07 +0200

    New conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Ghostty term info
+set -x TERM xterm-256color
 # Use starship prompt
 starship init fish | source
 
@@ -12,7 +14,7 @@ set -x PATH $PATH $GOROOT/bin $GOPATH/bin
 
 # Aliases
 alias e="exit"
-alias update="sudo dnf upgrade --refresh && sudo dnf system-upgrade download --releasever=41"
+alias update="sudo dnf upgrade --refresh && sudo dnf system-upgrade download"
 alias update_apt="sudo apt update && sudo apt upgrade"
 alias vs="code"
 alias cls="clear"
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -18,6 +18,26 @@ return {
             },
           },
         },
+        ty = {},
+      },
+      setup = {
+        ty = function(_, opts)
+          local lspconfig = require("lspconfig")
+          local configs = require("lspconfig.configs")
+          local util = require("lspconfig.util")
+
+          if not configs.ty then
+            configs.ty = {
+              default_config = {
+                cmd = { "ty", "server" },
+                filetypes = { "python" },
+                root_dir = util.root_pattern("pyproject.toml", ".git"),
+                single_file_support = true,
+              },
+            }
+          end
+          lspconfig.ty.setup(opts)
+        end,
       },
     },
   },
